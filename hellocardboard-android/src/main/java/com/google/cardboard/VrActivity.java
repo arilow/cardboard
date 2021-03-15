ba57b97f13c5bfce58569858d8434ed680533b66
arilow <arilow@creativa77.com.ar>
2021-03-15 16:52:34 -0300

    Added new line

--- hellocardboard-android/src/main/java/com/google/cardboard/VrActivity.java
+++ hellocardboard-android/src/main/java/com/google/cardboard/VrActivity.java
@@ -51,6 +51,7 @@ public class VrActivity extends AppCompatActivity implements PopupMenu.OnMenuIte
     System.loadLibrary("cardboard_jni");
   }
 
+  
   private static final String TAG = VrActivity.class.getSimpleName();
 
   // Permission request codes
